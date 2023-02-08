538f88bd0c4b80200e0c5e22a1701ffc10f03b58
leesibin <tlqls123@gmail.com>
2023-02-08 17:50:37 +0900

    2/08 gpt수업

--- Gpt_api.js
+++
@@ -1,7 +0,0 @@
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-    organization: "org-yR3ENrdqWba32qJxF7pmWKfa",
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
