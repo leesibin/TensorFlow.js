bbe03e15caf98af6b75562e2d50c646bd9b806bf
leesibin <tlqls123@gmail.com>
2023-03-06 14:50:52 +0900

    3/6 vuetf 수정 home

--- vue_tfjs/tsf02/src/views/HomeView.vue
+++ vue_tfjs/tsf02/src/views/HomeView.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="home">
+    <!-- 전환 기능을 넣은 구문 -->
     <button @click="select = 'USD'">달러</button>
     <button @click="select = 'JPY'">엔화</button>
     <keep-alive>
-      <component :is="JPY"></component>
+      <component :is="select"></component>
     </keep-alive>
-    <USD />
-    <JPY />
+    <!-- keep-alive 입력하면 입력값이 유지된다! -->
+
+    <!-- 그냥 한 번에 출력하는 구문 -->
+    <!-- <USD />
+    <br />
+    <JPY /> -->
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 // @ is an alias to /src
 import USD from '@/components/USD.vue'
 import JPY from '@/components/JPY.vue'
